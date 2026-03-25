f975a7c66d088a2aa0c9e34804d06f52aa306da3
Mikah Ho <mikah.ho@student.tdsb.on.ca>
2026-03-25 15:00:14 -0400

    www

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,61 @@
+/**
+ * File: Else if
+ * Author: Mikah Ho
+ * Date Created: Mar 25, 2026
+ * Date Last Modified: Mar 25, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	//q1
+	public static String evenOrOdd(int num) {
+	    
+	    if (num == 0) {
+	        return "Zero";
+	    }
+	    else if (num % 2 == 0) {
+	        return "Even";
+	    }
+	    else {
+	        return "Odd";
+	    }
+	}
 	
-
+	
+	//q2
+	public static String positiveOrNegative(int num) {
+	    
+	    if (num == 0) {
+	        return "Zero";
+	    }
+	    else if (num > 0) {
+	        return "Positive";
+	    }
+	    else {
+	        return "Negative";
+	    }
+	}
+	
+	//q3
+	public static String pluralize(String word) {
+	    
+	    word = word.toLowerCase();
+	    
+	    if (word.endsWith("ife")) {
+	        return "ives";
+	    }
+	    else if (word.endsWith("ey")) {
+	        return "eys";
+	    }
+	    else if (word.endsWith("y")) {
+	        return "ies";
+	    }
+	    else {
+	        return "s";
+		}
+	}
 }
